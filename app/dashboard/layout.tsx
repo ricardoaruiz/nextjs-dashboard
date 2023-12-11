82b7f23cab045965bbe2e6976c0ebd2ffafb6701
Ricardo Ruiz <ricardo.ruiz@grupoboticario.com.br>
2023-12-11 08:36:38 -0300

    fisrt code upload

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+import SideNav from '../ui/dashboard/sidenav';
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
